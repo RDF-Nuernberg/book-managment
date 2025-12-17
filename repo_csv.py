c0efd1f6503ca25cc4d190042e4e117593c47c86
rdf-mal <240441674+rdf-mal@users.noreply.github.com>
2025-12-17 12:49:36 +0100

    Quick fix to convert numbers to int or float

--- repo_csv.py
+++ repo_csv.py
@@ -1,11 +1,20 @@
 import csv # to handle CSV files
 
+def to_number(value):
+    try:
+        return int(value)
+    except ValueError:
+        try:
+            return float(value)
+        except ValueError:
+            return value
+        
 def load_books(filename):
     """Return all books from a CSV file as a list of dictionaries."""
     with open(filename) as csvfile:
         reader = csv.DictReader(csvfile, delimiter=';')
         # Return a list of dictionaries
-        return list(reader) 
+        return [{k: to_number(v) for k,v in book.items()} for book in reader]
     
 def save_books(filename, list_of_books):
     """Save all books to the CSV file.
